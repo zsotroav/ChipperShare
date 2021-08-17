304c15e7d2a16f6c563c6ba88e2b18741d237546
zsotroav <zsotroav@gmail.com>
2021-08-17 22:58:13 +0200

    Change TabIndexes in FormKey

--- ChipperShare/source/FormKey.Designer.cs
+++ ChipperShare/source/FormKey.Designer.cs
@@ -40,45 +40,13 @@ namespace ChipperShare
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // TextKeyName
-            // 
-            this.TextKeyName.Location = new System.Drawing.Point(0, 0);
-            this.TextKeyName.Name = "TextKeyName";
-            this.TextKeyName.Size = new System.Drawing.Size(100, 23);
-            this.TextKeyName.TabIndex = 8;
-            // 
             // TextKeyPass
             // 
             this.TextKeyPass.Location = new System.Drawing.Point(115, 12);
             this.TextKeyPass.Name = "TextKeyPass";
             this.TextKeyPass.Size = new System.Drawing.Size(203, 23);
-            this.TextKeyPass.TabIndex = 1;
+            this.TextKeyPass.TabIndex = 2;
             this.TextKeyPass.UseSystemPasswordChar = true;
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 7;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Key Passphrase";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 44);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 15);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Passphrase again";
             // 
             // TextKeyPass2
             // 
@@ -88,23 +56,41 @@ namespace ChipperShare
             this.TextKeyPass2.TabIndex = 4;
             this.TextKeyPass2.UseSystemPasswordChar = true;
             // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(223, 70);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(95, 23);
-            this.btnSave.TabIndex = 6;
-            this.btnSave.Text = "Submit";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
-            // 
             // comboIP
             // 
             this.comboIP.FormattingEnabled = true;
             this.comboIP.Location = new System.Drawing.Point(115, 70);
             this.comboIP.Name = "comboIP";
             this.comboIP.Size = new System.Drawing.Size(102, 23);
-            this.comboIP.TabIndex = 9;
+            this.comboIP.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(223, 70);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(95, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Submit";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key Passphrase";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Passphrase again";
             // 
             // label4
             // 
@@ -112,7 +98,7 @@ namespace ChipperShare
             this.label4.Location = new System.Drawing.Point(12, 73);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 15);
-            this.label4.TabIndex = 10;
+            this.label4.TabIndex = 5;
             this.label4.Text = "IP address";
             // 
             // FormKey
